12935a9595ebabfed89e1d9453787f0eb3536a8a
Ian Melendez <imelendez@preludedynamics.com>
2021-02-05 09:28:02 -0600

    added show instance where everything is parenthesized

--- src/Lambda/Syntax.hs
+++ src/Lambda/Syntax.hs
@@ -4,14 +4,40 @@ data Exp = Constant String
          | Variable Variable 
          | Apply Exp Exp 
          | Lambda String Exp
-         deriving Show
 
 data Variable = RawVar String 
               | FreeVar String 
               | BoundVar String
-              deriving Show
 
 varName :: Variable -> String 
 varName (RawVar n) = n
 varName (FreeVar n) = n
 varName (BoundVar n) = n
+
+instance Show Exp where 
+  show (Constant str) = str
+  show (Variable var) = show var
+  show (Apply expr expr') = "(" ++ show expr ++ ") (" ++ show expr' ++ ")"
+  show (Lambda name expr) = "(\\" ++ name ++ ". " ++ show expr ++ ")"
+
+instance Show Variable where 
+  show (RawVar var) = var
+  show (FreeVar var) = var
+  show (BoundVar var) = var
+
+
+
+infixr 5 :^:
+data Tree a =  Leaf a  |  Tree a :^: Tree a
+
+instance (Show a) => Show (Tree a) where
+
+       showsPrec d (Leaf m) = showParen (d > app_prec) $
+            showString "Leaf " . showsPrec (app_prec+1) m
+         where app_prec = 10
+
+       showsPrec d (u :^: v) = showParen (d > up_prec) $
+            showsPrec (up_prec+1) u .
+            showString " :^: "      .
+            showsPrec (up_prec+1) v
+         where up_prec = 5

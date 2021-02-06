9518c3f7c978a79180b0b2cc24e57cdf456946c2
Ian Melendez <imelendez@preludedynamics.com>
2021-02-06 11:38:21 -0600

    refactored prettification to use a monad

--- src/Lambda/Syntax.hs
+++ src/Lambda/Syntax.hs
@@ -4,6 +4,7 @@ import qualified Data.Text as T
 import Prettyprinter
 import Prettyprinter.Util
 import Prettyprinter.Render.Terminal
+import Control.Monad.State.Lazy
 
 data Exp = Constant String 
          | Variable Variable 
@@ -47,6 +48,17 @@ data AParen = AParenYellow
             | AParenCyan
             deriving (Show)
 
+data ParenState = ParenState {
+    parenPrec :: Int,
+    parenStyle :: AParen
+  }
+initParenState :: ParenState
+initParenState = ParenState 0 AParenMagenta
+
+type LambdaDoc = Doc LambdaAnn
+type PrettyExp = State ParenState
+type ParenWrapper = (LambdaDoc -> LambdaDoc)
+
 ansiPrettyExp :: Exp -> SimpleDocStream AnsiStyle
 ansiPrettyExp = reAnnotateS ansiStyle . prettyExp
   where
@@ -66,7 +78,44 @@ prettyExp :: Exp -> SimpleDocStream LambdaAnn
 prettyExp = layoutPretty defaultLayoutOptions . prettyExpDoc
 
 prettyExpDoc :: Exp -> Doc LambdaAnn
-prettyExpDoc = prettyExpDocPrec AParenMagenta 0
+prettyExpDoc expr = evalState (sPretty expr) initParenState 
+
+sPretty :: Exp -> PrettyExp LambdaDoc
+sPretty (Constant c) = pure $ annStr AConstant c
+sPretty (Variable var) = pure $ prettyVar var
+sPretty (Lambda var e) = do wrapper <- getParenWrapper 5 
+                            ePretty <- setPrec 0 >> sPretty e
+                            pure $ wrapper $ backslash
+                                           <> ann ABoundVar (annStr ABoundVar var) 
+                                           <> dot 
+                                           <+> ePretty
+sPretty (Apply e e') = do wrapper <- getParenWrapper 10
+                          ePretty <- setPrec 6 >> sPretty e
+                          ePretty' <- setPrec 11 >> sPretty e'
+                          pure $ wrapper $ ePretty <+> ePretty'
+
+setPrec :: Int -> PrettyExp ()
+setPrec prec = modify (\ps -> ps { parenPrec = prec })
+
+-- | main reason for using state, so we can get the paren wrapper and update the paren state
+-- | in one swoop (still probably too obfuscated to be worth it)
+getParenWrapper :: Int -> PrettyExp ParenWrapper
+getParenWrapper prec = do pPrec <- gets parenPrec
+                          if pPrec <= prec
+                            then pure id 
+                            else getWrapper
+  where 
+    getWrapper :: PrettyExp ParenWrapper
+    getWrapper = annParens . AParen <$> (gets parenStyle <* modify updateParenStyle)
+
+-- getParenWrapper' :: PrettyExp ParenWrapper 
+-- getParenWrapper' = do pStyle <- gets parenStyle
+--                      modify updateParenStyle
+--                      pure $ annParens (AParen pStyle)
+
+updateParenStyle :: ParenState -> ParenState
+updateParenStyle pState@ParenState { parenStyle = style } 
+  = pState { parenStyle = nextParenAnn style }
 
 prettyExpDocPrec :: AParen -> Int -> Exp -> Doc LambdaAnn
 prettyExpDocPrec _ _ (Constant c) = annStr AConstant c

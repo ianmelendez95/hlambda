bae0ab64feb36fdd988701bfc5b0e50ca92dbb22
Ian Melendez <imelendez@preludedynamics.com>
2021-03-13 12:00:35 -0600

    added patt tree merging

--- src/Miranda/PattMatch.hs
+++ src/Miranda/PattMatch.hs
@@ -1,11 +1,4 @@
-module Miranda.PattMatch
-  (     
-    test_def1,
-    test_def2,
-    test_def3,
-
-    mkTree
-  ) where 
+module Miranda.PattMatch where 
 
 import Control.Monad.State.Lazy
 
@@ -18,6 +11,27 @@ data PattTree = PTConstant (Numd S.Constant) PattTree
               | PTConstructor (Numd E.Constructor) PattTree
               | PTExp E.Exp
               deriving Show
+
+data MatchTree = MTConstant Int [S.Constant] MatchTree
+               | MTVariable Int [S.Variable] MatchTree
+               | MTConstructor Int [E.Constructor] MatchTree
+               | MTFatBar MatchTree MatchTree
+               | MTExp E.Exp
+               deriving Show
+
+--------------------------------------------------------------------------------
+-- The 'numbering' monad
+
+data Numd a = Numd Int a
+            deriving Show
+
+type NumdM = State Int
+
+numdNextN :: NumdM Int
+numdNextN = state (\n -> (n, n+1))
+
+numdNext :: a -> NumdM (Numd a)
+numdNext x = (`Numd` x) <$> numdNextN
 
 --------------------------------------------------------------------------------
 -- Test Input
@@ -54,20 +68,6 @@ test_tree3 :: PattTree
 test_tree3 = uncurry mkTree test_def3
 
 --------------------------------------------------------------------------------
--- The 'numbering' monad
-
-data Numd a = Numd Int a
-            deriving Show
-
-type NumdM = State Int
-
-numdNextN :: NumdM Int
-numdNextN = state (\n -> (n, n+1))
-
-numdNext :: a -> NumdM (Numd a)
-numdNext x = (`Numd` x) <$> numdNextN
-
---------------------------------------------------------------------------------
 -- Making the 'pattern tree'
 
 mkTree :: [E.Pattern] -> E.Exp -> PattTree
@@ -94,3 +94,31 @@ mkTree' (Numd n p:ps) e =
     (E.PConstructor c cps) -> 
       do numd_cps <- mapM numdNext cps
          PTConstructor (Numd n c) <$> mkTree' (numd_cps ++ ps) e
+
+--------------------------------------------------------------------------------
+-- merging into 'match tree'
+
+pattToMatchTree :: PattTree -> MatchTree
+pattToMatchTree (PTConstant (Numd n c) t) = MTConstant n [c] (pattToMatchTree t)
+pattToMatchTree (PTConstructor (Numd n c) t) = MTConstructor n [c] (pattToMatchTree t)
+pattToMatchTree (PTVariable (Numd n v) t) = MTVariable n [v] (pattToMatchTree t)
+pattToMatchTree (PTExp e) = MTExp e
+
+mergePTrees :: PattTree -> PattTree -> MatchTree
+mergePTrees p1@(PTVariable (Numd n1 v1) t1) p2@(PTVariable (Numd n2 v2) t2) = 
+  MTVariable (assertEqual (treeDivergeMsg p1 p2) n1 n2) [v1, v2] (mergePTrees t1 t2)
+
+mergePTrees p1@(PTConstructor (Numd n1 c1) t1) p2@(PTConstructor (Numd n2 c2) t2) = 
+  MTConstructor (assertEqual (treeDivergeMsg p1 p2) n1 n2) [c1, c2] (mergePTrees t1 t2)
+
+mergePTrees p1@(PTConstant (Numd n1 c1) t1) p2@(PTConstant (Numd n2 c2) t2) = 
+  MTConstant (assertEqual (treeDivergeMsg p1 p2) n1 n2) [c1, c2] (mergePTrees t1 t2)
+
+mergePTrees (PTExp e) (PTExp _) = MTExp e
+mergePTrees p1 p2 = MTFatBar (pattToMatchTree p1) (pattToMatchTree p2)
+
+treeDivergeMsg :: PattTree -> PattTree -> String
+treeDivergeMsg p1 p2 = "Pattern trees diverged in number: " ++ show p1 ++ " =/= " ++ show p2
+
+assertEqual :: (Eq a) => String -> a -> a -> a
+assertEqual msg x y = if x == y then x else error msg

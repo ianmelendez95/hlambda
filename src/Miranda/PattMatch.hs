c8f1e1b21a138e86ea73fbc00216ce77f9d91711
Ian Melendez <imelendez@preludedynamics.com>
2021-04-25 12:01:57 -0500

    added patt match compiler docs

--- src/Miranda/PattMatch.hs
+++ src/Miranda/PattMatch.hs
@@ -15,6 +15,12 @@ import Lambda.Constructor
 --------------------------------------------------------------------------------
 -- Interface
 
+-- | Compiles pattern equations to enriched expressions
+-- |
+-- | Guarantees about Case:
+-- |   all case expressions are _complete_, meaning all constructors of the type
+-- |   have a corresponding clause, and all clauses are constructors of the same type
+-- |   TODO - find out how to prove this by construction
 patternEquationsToEnriched :: [([E.Pattern], E.Exp)] -> E.Exp
 patternEquationsToEnriched patt_eqs 
   | not . allEqual $ map (length . fst) patt_eqs = 
